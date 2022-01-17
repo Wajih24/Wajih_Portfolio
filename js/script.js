e424bc842783b20ad5fe9336be07eee24646718a
Wajih Hamrouni <74443451+Wajih24@users.noreply.github.com>
2022-01-17 03:36:20 +0100

    Version Beta

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing", {
+    strings: ["", "Python Developer", "Freelancer", "Software Engineering student", "Python Trainer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
